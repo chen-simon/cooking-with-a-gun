b2b89523f6a8375ca4d1d7ba334ccdd5a5ef308e
Simon Chen <simonchen.sc.2002@gmail.com>
2024-09-19 13:21:22 -0400

    HID changes

--- Assets/Plugins/JoyconLib_scripts/HIDapi.cs
+++ Assets/Plugins/JoyconLib_scripts/HIDapi.cs
@@ -6,60 +6,131 @@ using System.Text;
 
 
 public class HIDapi {
+    private const string PRIMARY_DLL = "hidapi";
+    private const string FALLBACK_DLL = "libhidapi";
+    private static bool primaryLoaded = true;
 
-    [DllImport("hidapi")]
-    public static extern int hid_init();
+    // Helper to try invoking the function from the primary and fallback DLLs
+    private static T TryInvoke<T>(Func<T> primary, Func<T> fallback)
+    {
+        try
+        {
+            if (primaryLoaded) return primary();
+        }
+        catch (DllNotFoundException)
+        {
+            primaryLoaded = false;
+        }
+        return fallback();
+    }
 
-    [DllImport("hidapi")]
-    public static extern int hid_exit();
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_init", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_init_primary();
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_init", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_init_fallback();
+    public static int hid_init() => TryInvoke(hid_init_primary, hid_init_fallback);
 
-    [DllImport("hidapi")]
-    public static extern IntPtr hid_error(IntPtr device);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_exit", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_exit_primary();
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_exit", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_exit_fallback();
+    public static int hid_exit() => TryInvoke(hid_exit_primary, hid_exit_fallback);
 
-    [DllImport("hidapi")]
-    public static extern IntPtr hid_enumerate(ushort vendor_id, ushort product_id);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_error", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr hid_error_primary(IntPtr device);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_error", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr hid_error_fallback(IntPtr device);
+    public static IntPtr hid_error(IntPtr device) => TryInvoke(() => hid_error_primary(device), () => hid_error_fallback(device));
 
-    [DllImport("hidapi")]
-    public static extern void hid_free_enumeration(IntPtr devs);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_enumerate", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr hid_enumerate_primary(ushort vendor_id, ushort product_id);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_enumerate", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr hid_enumerate_fallback(ushort vendor_id, ushort product_id);
+    public static IntPtr hid_enumerate(ushort vendor_id, ushort product_id) => TryInvoke(() => hid_enumerate_primary(vendor_id, product_id), () => hid_enumerate_fallback(vendor_id, product_id));
 
-    [DllImport("hidapi")]
-    public static extern int hid_get_feature_report(IntPtr device, byte[] data, UIntPtr length);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_free_enumeration", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void hid_free_enumeration_primary(IntPtr devs);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_free_enumeration", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void hid_free_enumeration_fallback(IntPtr devs);
+    public static void hid_free_enumeration(IntPtr devs) => TryInvoke(() => { hid_free_enumeration_primary(devs); return 0; }, () => { hid_free_enumeration_fallback(devs); return 0; });
 
-    [DllImport("hidapi")]
-    public static extern int hid_get_indexed_string(IntPtr device, int string_index, StringBuilder str, UIntPtr maxlen);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_get_feature_report", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_get_feature_report_primary(IntPtr device, byte[] data, UIntPtr length);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_get_feature_report", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_get_feature_report_fallback(IntPtr device, byte[] data, UIntPtr length);
+    public static int hid_get_feature_report(IntPtr device, byte[] data, UIntPtr length) => TryInvoke(() => hid_get_feature_report_primary(device, data, length), () => hid_get_feature_report_fallback(device, data, length));
 
-    [DllImport("hidapi")]
-    public static extern int hid_get_manufacturer_string(IntPtr device, StringBuilder str, UIntPtr maxlen);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_get_indexed_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_get_indexed_string_primary(IntPtr device, int string_index, StringBuilder str, UIntPtr maxlen);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_get_indexed_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_get_indexed_string_fallback(IntPtr device, int string_index, StringBuilder str, UIntPtr maxlen);
+    public static int hid_get_indexed_string(IntPtr device, int string_index, StringBuilder str, UIntPtr maxlen) => TryInvoke(() => hid_get_indexed_string_primary(device, string_index, str, maxlen), () => hid_get_indexed_string_fallback(device, string_index, str, maxlen));
 
-    [DllImport("hidapi")]
-    public static extern int hid_get_product_string(IntPtr device, StringBuilder str, UIntPtr maxlen);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_get_manufacturer_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_get_manufacturer_string_primary(IntPtr device, StringBuilder str, UIntPtr maxlen);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_get_manufacturer_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_get_manufacturer_string_fallback(IntPtr device, StringBuilder str, UIntPtr maxlen);
+    public static int hid_get_manufacturer_string(IntPtr device, StringBuilder str, UIntPtr maxlen) => TryInvoke(() => hid_get_manufacturer_string_primary(device, str, maxlen), () => hid_get_manufacturer_string_fallback(device, str, maxlen));
 
-    [DllImport("hidapi")]
-    public static extern int hid_get_serial_number_string(IntPtr device, StringBuilder str, UIntPtr maxlen);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_get_product_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_get_product_string_primary(IntPtr device, StringBuilder str, UIntPtr maxlen);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_get_product_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_get_product_string_fallback(IntPtr device, StringBuilder str, UIntPtr maxlen);
+    public static int hid_get_product_string(IntPtr device, StringBuilder str, UIntPtr maxlen) => TryInvoke(() => hid_get_product_string_primary(device, str, maxlen), () => hid_get_product_string_fallback(device, str, maxlen));
 
-    [DllImport("hidapi")]
-    public static extern IntPtr hid_open(ushort vendor_id, ushort product_id, string serial_number);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_get_serial_number_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_get_serial_number_string_primary(IntPtr device, StringBuilder str, UIntPtr maxlen);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_get_serial_number_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_get_serial_number_string_fallback(IntPtr device, StringBuilder str, UIntPtr maxlen);
+    public static int hid_get_serial_number_string(IntPtr device, StringBuilder str, UIntPtr maxlen) => TryInvoke(() => hid_get_serial_number_string_primary(device, str, maxlen), () => hid_get_serial_number_string_fallback(device, str, maxlen));
 
-    [DllImport("hidapi")]
-    public static extern void hid_close(IntPtr device);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_open", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr hid_open_primary(ushort vendor_id, ushort product_id, string serial_number);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_open", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr hid_open_fallback(ushort vendor_id, ushort product_id, string serial_number);
+    public static IntPtr hid_open(ushort vendor_id, ushort product_id, string serial_number) => TryInvoke(() => hid_open_primary(vendor_id, product_id, serial_number), () => hid_open_fallback(vendor_id, product_id, serial_number));
 
-    [DllImport("hidapi")]
-    public static extern IntPtr hid_open_path(string path);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_close", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void hid_close_primary(IntPtr device);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_close", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void hid_close_fallback(IntPtr device);
+    public static void hid_close(IntPtr device) => TryInvoke(() => { hid_close_primary(device); return 0; }, () => { hid_close_fallback(device); return 0; });
 
-    [DllImport("hidapi")]
-    public static extern int hid_read(IntPtr device, byte[] data, UIntPtr length);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_open_path", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr hid_open_path_primary(string path);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_open_path", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr hid_open_path_fallback(string path);
+    public static IntPtr hid_open_path(string path) => TryInvoke(() => hid_open_path_primary(path), () => hid_open_path_fallback(path));
 
-    [DllImport("hidapi")]
-    public static extern int hid_read_timeout(IntPtr dev, byte[] data, UIntPtr length, int milliseconds);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_read", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_read_primary(IntPtr device, byte[] data, UIntPtr length);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_read", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_read_fallback(IntPtr device, byte[] data, UIntPtr length);
+    public static int hid_read(IntPtr device, byte[] data, UIntPtr length) => TryInvoke(() => hid_read_primary(device, data, length), () => hid_read_fallback(device, data, length));
 
-    [DllImport("hidapi")]
-    public static extern int hid_send_feature_report(IntPtr device, byte[] data, UIntPtr length);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_read_timeout", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_read_timeout_primary(IntPtr dev, byte[] data, UIntPtr length, int milliseconds);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_read_timeout", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_read_timeout_fallback(IntPtr dev, byte[] data, UIntPtr length, int milliseconds);
+    public static int hid_read_timeout(IntPtr dev, byte[] data, UIntPtr length, int milliseconds) => TryInvoke(() => hid_read_timeout_primary(dev, data, length, milliseconds), () => hid_read_timeout_fallback(dev, data, length, milliseconds));
 
-    [DllImport("hidapi")]
-    public static extern int hid_set_nonblocking(IntPtr device, int nonblock);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_send_feature_report", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_send_feature_report_primary(IntPtr device, byte[] data, UIntPtr length);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_send_feature_report", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_send_feature_report_fallback(IntPtr device, byte[] data, UIntPtr length);
+    public static int hid_send_feature_report(IntPtr device, byte[] data, UIntPtr length) => TryInvoke(() => hid_send_feature_report_primary(device, data, length), () => hid_send_feature_report_fallback(device, data, length));
 
-    [DllImport("hidapi")]
-    public static extern int hid_write(IntPtr device, byte[] data, UIntPtr length);
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_set_nonblocking", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_set_nonblocking_primary(IntPtr device, int nonblock);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_set_nonblocking", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_set_nonblocking_fallback(IntPtr device, int nonblock);
+    public static int hid_set_nonblocking(IntPtr device, int nonblock) => TryInvoke(() => hid_set_nonblocking_primary(device, nonblock), () => hid_set_nonblocking_fallback(device, nonblock));
+
+    [DllImport(PRIMARY_DLL, EntryPoint = "hid_write", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_write_primary(IntPtr device, byte[] data, UIntPtr length);
+    [DllImport(FALLBACK_DLL, EntryPoint = "hid_write", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int hid_write_fallback(IntPtr device, byte[] data, UIntPtr length);
+    public static int hid_write(IntPtr device, byte[] data, UIntPtr length) => TryInvoke(() => hid_write_primary(device, data, length), () => hid_write_fallback(device, data, length));
 }
 
 struct hid_device_info {
---
+++ Assets/Scripts/JoyconController.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyconController : MonoBehaviour {
+	
+	private List<Joycon> joycons;
+
+    // Values made available via Unity
+    public float[] stick;
+    public Vector3 gyro;
+    public Vector3 accel;
+    public int jc_ind = 0;
+    public Quaternion orientation;
+    private Quaternion aim_offset;
+
+    public AudioSource gunshot_clip;
+
+	public GameObject destroyedPrefab;
+	
+	public RectTransform pointer;
+    void Start ()
+    {
+        gyro = new Vector3(0, 0, 0);
+        accel = new Vector3(0, 0, 0);
+        aim_offset = Quaternion.identity;
+        // get the public Joycon array attached to the JoyconManager in scene
+        joycons = JoyconManager.Instance.j;
+		if (joycons.Count < jc_ind+1){
+			Destroy(gameObject);
+		}
+		
+		gameObject.transform.rotation = orientation;
+	}
+
+    // Update is called once per frame
+    void Update () {
+		// make sure the Joycon only gets checked if attached
+		if (joycons.Count > 0)
+        {
+			Joycon j = joycons [jc_ind];
+			// GetButtonDown checks if a button has been pressed (not held)
+            if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
+            {
+				// Debug.Log ("Right trigger pressed");
+				joycons[jc_ind].SetRumble(160, 320, 0.6f, 100);  // Short 
+				gunshot_clip.Play();
+            }
+			// GetButtonDown checks if a button has been released
+			if (j.GetButtonUp (Joycon.Button.SHOULDER_2))
+			{
+				// Debug.Log ("Right trigger released");
+			}
+			// GetButtonDown checks if a button is currently down (pressed or held)
+			if (j.GetButton (Joycon.Button.SHOULDER_2))
+			{
+				// Debug.Log ("Right trigger held");
+			}
+
+			if (j.GetButtonDown(Joycon.Button.DPAD_RIGHT))
+            {
+				Debug.Log ("DPAD_RIGHT pressed");
+				// GetStick returns a 2-element vector with x/y joystick components
+				Debug.Log(string.Format("Stick x: {0:N} Stick y: {1:N}",j.GetStick()[0],j.GetStick()[1]));
+            
+				// Joycon has no magnetometer, so it cannot accurately determine its yaw value. Joycon.Recenter allows the user to reset the yaw value.
+				j.Recenter ();
+			}
+			// GetButtonDown checks if a button has been released
+			if (j.GetButtonUp (Joycon.Button.DPAD_RIGHT))
+			{
+				Debug.Log ("DPAD_RIGHT released");
+			}
+			// GetButtonDown checks if a button is currently down (pressed or held)
+			if (j.GetButton (Joycon.Button.DPAD_RIGHT))
+			{
+				Debug.Log ("DPAD_RIGHT held");
+			}
+
+			if (j.GetButtonDown (Joycon.Button.DPAD_DOWN)) {
+				Debug.Log ("Rumble");
+
+				// Rumble for 200 milliseconds, with low frequency rumble at 160 Hz and high frequency rumble at 320 Hz. For more information check:
+				// https://github.com/dekuNukem/Nintendo_Switch_Reverse_Engineering/blob/master/rumble_data_table.md
+
+				j.SetRumble (160, 320, 0.6f, 200);
+
+				// The last argument (time) in SetRumble is optional. Call it with three arguments to turn it on without telling it when to turn off.
+                // (Useful for dynamically changing rumble values.)
+				// Then call SetRumble(0,0,0) when you want to turn it off.
+			}
+
+            stick = j.GetStick();
+
+            // Gyro values: x, y, z axis values (in radians per second)
+            gyro = j.GetGyro();
+
+            // Accel values:  x, y, z axis values (in Gs)
+            accel = j.GetAccel();
+            Debug.Log(accel);
+            
+            orientation = j.GetVector();
+            
+            if (j.GetButton(Joycon.Button.DPAD_UP)){
+				gameObject.GetComponent<Renderer>().material.color = Color.red;
+			}
+			else
+			{
+				gameObject.GetComponent<Renderer>().material.color = Color.blue;
+			}
+
+            Quaternion desiredRot = orientation;
+            
+            // Map orientation to screen pointer movement
+            UpdatePointerPosition(orientation);
+            
+            if (j.GetButtonDown(Joycon.Button.DPAD_RIGHT))
+			{
+				aim_offset = Quaternion.Inverse(desiredRot);
+			}
+			
+			gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, aim_offset * desiredRot, 20f * Time.deltaTime);
+			
+			// https://github.com/Looking-Glass/JoyconLib/issues/8
+			// gameObject.transform.Rotate(90,0,0,Space.World); 
+			// Debug.Log(gameObject.transform.rotation);
+        }
+	    
+    }
+    
+    // Method to map the Joycon orientation to the pointer's position on the screen
+    void UpdatePointerPosition(Quaternion joyconOrientation)
+    {
+	    // Convert the orientation to a forward vector (direction the Joycon is pointing)
+	    Vector3 forward = joyconOrientation * Vector3.forward;
+
+	    // You can use the x and y axes from the forward vector to calculate pointer position
+	    // Map these values to the screen (e.g., multiplying by some sensitivity factor)
+	    float pointerSensitivity = 300f;  // Adjust this value to fine-tune the pointer speed
+
+	    // Assuming you want to move the pointer in 2D space (x and y coordinates of the screen)
+	    float newX = forward.x * pointerSensitivity;
+	    float newY = forward.y * pointerSensitivity;
+
+	    // Set the pointer's anchored position (used for UI elements in Canvas)
+	    // pointer.anchoredPosition = new Vector2(newX, newY);
+    }
+}
