92590db22ab317e2e4082ce27d53649bc83db312
Simon Chen <simonchen.sc.2002@gmail.com>
2024-09-19 15:20:11 -0400

    Working Gun Model

--- Assets/Scripts/Shooting/GunController.cs
+++ Assets/Scripts/Shooting/GunController.cs
@@ -7,6 +7,8 @@ public class GunController : MonoBehaviour
     public static GunController main;
 
     public RectTransform crosshair;
+    public Transform gunModel;
+    public float distance;
     
     void Awake()
     {
@@ -22,11 +24,21 @@ public class GunController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateGunModelRotation();
     }
 
     public void UpdateCrosshairPostiton(Vector2 screenPosition)
     {
         crosshair.anchoredPosition = screenPosition;
     }
+
+    void UpdateGunModelRotation()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(crosshair.anchoredPosition + new Vector2(Screen.width / 2, Screen.height / 2));
+        Vector3 target = ray.direction * distance + Camera.main.transform.position;
+
+        Vector3 targetDirection = gunModel.transform.position - target;
+        Vector3 newDirection = Vector3.RotateTowards(gunModel.forward, targetDirection, Mathf.PI, 0);
+        gunModel.transform.rotation = Quaternion.LookRotation(newDirection);
+    }    
 }
