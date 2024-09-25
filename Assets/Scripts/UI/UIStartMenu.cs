c3919149a82581d6369238d6c9e6bff64e5a4aad
Simon Chen <simonchen.sc.2002@gmail.com>
2024-09-25 17:54:40 -0400

    Build ready

---
+++ Assets/Scripts/UI/UIStartMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIStartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        Screen.SetResolution(1366, 768, FullScreenMode.FullScreenWindow);
+        SceneManager.LoadScene("Template");
+    }
+}
