2b5a2c6707bd4a4159f8bb14e3f8faf4d745e402
Simon Chen <simonchen.sc.2002@gmail.com>
2024-10-23 11:24:10 -0400

    Update menu settings

--- Assets/Scripts/UI/UIStartMenu.cs
+++ Assets/Scripts/UI/UIStartMenu.cs
@@ -14,12 +14,12 @@ public class UIStartMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Hard code
+        if (Input.GetKeyDown(KeyCode.Mouse0)) Play();
     }
 
     public void Play()
     {
-        Screen.SetResolution(1366, 768, FullScreenMode.FullScreenWindow);
         SceneManager.LoadScene("Template");
     }
 }
